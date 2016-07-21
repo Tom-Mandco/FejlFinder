59fb5d01254a5312933e08f6a6ecff9eb4cb2efc
Tom Smith <TSmith@Mackays.co.uk>
2016-07-21 11:47:41 +0100

    Initial commit.

---
+++ FejlFinder/AppForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FejlFinder
+{
+    public partial class AppForm : Form
+    {
+        public AppForm()
+        {
+            InitializeComponent();
+        }
+        #region På begivenhed
+        private void btnOpfriske_Click(object sender, EventArgs e)
+        {
+            Løb();
+        }
+        
+        #endregion
+
+
+        #region Hovedbegivenhed
+        private void Løb()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ FejlFinder/ApplicationModule.cs
@@ -0,0 +1,23 @@
+﻿namespace FejlFinder
+{
+    using Ninject.Modules;
+    using NLog;
+    using Interfaces;
+    using Classes;
+    using System.Configuration;
+    using System;
+
+    class ApplicationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ILog>().ToMethod(x =>
+            {
+                var scope = x.Request.ParentRequest.Service.FullName;
+                var log = (ILog)LogManager.GetLogger(scope, typeof(Log));
+                return log;
+            });
+            Bind<IDataHandler>().To<DataHandler>();
+        }
+    }
+}
