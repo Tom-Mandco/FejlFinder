fa698a1a0c87325dd12c83f536406d9ffaeb8810
Tom Smith <TSmith@Mackays.co.uk>
2016-08-19 17:24:38 +0100

    updating origin branch with latest

--- FejlFinder/Klasser/FilHandleren.cs
+++ FejlFinder/Klasser/FilHandleren.cs
@@ -4,5 +4,24 @@
 
     class FilHandleren : IFilHandleren
     {
+        ILog logger;
+        IFilLæser filLæser;
+        IFilPlacering filPlacering;
+
+        public FilHandleren(ILog logger, IFilLæser filLæser, IFilPlacering filPlacering)
+        {
+            this.logger = logger;
+            this.filLæser = filLæser;
+            this.filPlacering = filPlacering;
+        }
+
+        public void LøbFilfinder(string filSti)
+        {
+            logger.Info("Løb Fil Finder.");
+
+            
+
+            logger.Info("Fil Finder Sluttede.");
+        }
     }
 }
